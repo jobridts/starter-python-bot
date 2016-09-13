f450d0f40d196a636947b75df126bd7386e34880
Jo Bridts <jo@jobridts.be>
2016-09-13 14:02:52 +0200

    Intro + description

--- bot/messenger.py
+++ bot/messenger.py
@@ -40,7 +40,9 @@ class Messenger(object):
     def write_help_message(self, channel_id):
         bot_uid = self.clients.bot_user_id()
         txt = '{}\n{}\n{}\n{}'.format(
-            "I'm your friendly Slack bot written in Python.  I'll *_respond_* to the following commands:",
+            "Hallo. Ik ben de LPA-slack-robot.  Ik *_antwoord_* op volgende vragen:",
+            "> `<@" + bot_uid + "> menu` - Ik zoek op welke soep er vandaag in Digipolis wordt geserveerd",
+            "> `<@" + bot_uid + "> suggestie` - Ik zoek op wat het broodje van de week is",
             "> `hi <@" + bot_uid + ">` - I'll respond with a randomized greeting mentioning your user. :wave:",
             "> `<@" + bot_uid + "> joke` - I'll tell you one of my finest jokes, with a typing pause for effect. :laughing:",
             "> `<@" + bot_uid + "> attachment` - I'll demo a post with an attachment using the Web API. :paperclip:")
